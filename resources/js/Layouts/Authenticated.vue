02efb6a34d1a57550e1e4205e6ccf87e8052de50
nasim anwari <nasimanwari367@gmail.com>
2022-09-18 19:04:08 +0200

    first commit

---
+++ resources/js/Layouts/Authenticated.vue
@@ -0,0 +1,5 @@
+
+<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+    <BreezeNavLink :href="route('dashboard')" :active="route().current('dashboard')">Dashboard</BreezeNavLink>
+    <BreezeNavLink :href="route('posts.index')" :active="route().current('posts.index')">Posts</BreezeNavLink>
+  </div>
